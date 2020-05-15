58f892e5157a9d32ad476713314f4d05f53f336a
saraswatiti <saraswati.rigorousweb@gmail.com>
2020-05-15 17:36:55 +0545

    add preloader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import Header from "./Components/Header";
-import "./App";
+import { CubeGrid } from "styled-loaders-react";
 import Footer from "./Components/Footer";
 const Homepage = React.lazy(() => import("./Components/Homepage"));
 const SinglePage = React.lazy(() => import("./Components/SinglePage"));
 function App() {
+  const [loadering, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
     <div className="App">
-      <HashRouter basename="/">
-        <Header />
-        <React.Suspense fallback={"loading..........."}>
-          <Switch>
-            <Route path="/" exact component={Homepage} />
-            <Route path="/movies/:id" component={SinglePage} />
-          </Switch>
-        </React.Suspense>
-        <Footer />
-      </HashRouter>
+      {loadering ? (
+        <div className="loading">
+          <CubeGrid color="red" size="60px" duration="1.5s" />
+        </div>
+      ) : (
+        <HashRouter basename="/">
+          <Header />
+          <React.Suspense fallback={"loading..........."}>
+            <Switch>
+              <Route path="/" exact component={Homepage} />
+              <Route path="/movies/:id" component={SinglePage} />
+            </Switch>
+          </React.Suspense>
+          <Footer />
+        </HashRouter>
+      )}
     </div>
   );
 }
